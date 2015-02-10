02b5de6f614d87fdb523aa967eb82a061b625577
Jason Angell <waterboy94@gmail.com>
2015-02-10 17:51:49 -0500

    see last commit

---
+++ prob2.py
@@ -0,0 +1,40 @@
+# Jason Angell
+# Therm & Stat
+# 2/10/2015
+
+def printos(arrayO):
+    print '[',
+    for i in range(len(arrayO)):
+        for j in range(arrayO[i]):
+            print '*',
+        if arrayO[i] == 0:
+            print ' ',
+        if i < len(arrayO) - 1:
+            print '|',
+    print ']'
+
+def energytree(n, o):
+    # n is energy, o is oscillators
+    if n == 0:
+        printos(o)
+        return
+    if n == 1:
+        for i in range(len(o)):
+            o[i] += 1
+            printos(o)
+            o[i] -= 1
+    else:
+        for j in range(len(o)):
+            o[j] += 1
+            energytree(n - 1, o)
+            o[j] -= 1
+
+def runenergy(energy, size):
+    a = []
+    for n in range(size):
+        a.append(0)
+    energytree(energy, a)
+
+# to run with arbitrary energy and oscillators:
+# replace 1 with the energy and 3 with the number of oscillators you want
+runenergy(1, 3) 
